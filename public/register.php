c77d8e3107d5ef180c281fadd1a37aeebd6b8bcb
joshuaobare <jobare.obare@gmail.com>
2024-06-12 20:03:14 +0300

    Add error message spans to registration page

--- public/register.php
+++ public/register.php
@@ -21,39 +21,93 @@ if ((isset($_POST["first_name"])) && (isset($_POST["last_name"])) && (isset($_PO
 }
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../styles/main.css" />
+    <link rel="stylesheet" href="../styles/register.css" />
+    <title>Register</title>
+  </head>
 
-<head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../styles/main.css">
-        <link rel="stylesheet" href="../styles/register.css">
-        <title>Register</title>
-</head>
-
-<body>
-        <main class="register-main">
-                <form action="#" class="register-form" method="post">
-                        <div class="register-form-item"><label for="first_name">First Name</label><input type="text"
-                                        class="register-form-item-input" name="first_name" id="first_name"></div>
-                        <div class="register-form-item"><label for="last_name">Last Name</label><input type="text"
-                                        class="register-form-item-input" name="last_name" id="last_name"></div>
-                        <div class="register-form-item"><label for="date_of_birth">Date of Birth</label><input
-                                        type="date" class="register-form-item-input" name="date_of_birth"
-                                        id="date_of_birth"></div>
-                        <div class="register-form-item"><label for="email">Email</label><input type="email"
-                                        class="register-form-item-input" name="email" id="email">
-                        </div>
-                        <div class="register-form-item"><label for="password">Password</label><input type="password"
-                                        class="register-form-item-input" name="password" id="password"></div>
-                        <div class="register-form-item"><label for="confirm_password">Confirm Password</label><input
-                                        type="password" class="register-form-item-input" name="confirm_password"
-                                        id="confirm_password"></div>
-                        <button>Submit</button>
-                </form>
-        </main>
-
-</body>
-
+  <body>
+    <main class="register-main">
+      <form action="#" class="register-form" method="post">
+        <div class="register-form-item">
+          <label for="first_name">First Name</label>
+          <div class="register-form-input-cont">
+            <input
+              type="text"
+              class="register-form-item-input"
+              name="first_name"
+              id="first_name"
+            />
+            <span class="error-message"></span>
+          </div>
+        </div>
+        <div class="register-form-item">
+          <label for="last_name">Last Name</label>
+          <div class="register-form-input-cont">
+            <input
+              type="text"
+              class="register-form-item-input"
+              name="last_name"
+              id="last_name"
+            />
+            <span class="error-message">Error message</span>
+          </div>
+        </div>
+        <div class="register-form-item">
+          <label for="date_of_birth">Date of Birth</label>
+          <div class="register-form-input-cont">
+            <input
+              type="date"
+              class="register-form-item-input"
+              name="date_of_birth"
+              id="date_of_birth"
+            /><span class="error-message"></span>
+          </div>
+        </div>
+        <div class="register-form-item">
+          <label for="email">Email</label>
+          <div class="register-form-input-cont">
+            <input
+              type="email"
+              class="register-form-item-input"
+              name="email"
+              id="email"
+            />
+            <span class="error-message"></span>
+          </div>
+        </div>
+        <div class="register-form-item">
+          <label for="password">Password</label>
+          <div class="register-form-input-cont">
+            <input
+              type="password"
+              class="register-form-item-input"
+              name="password"
+              id="password"
+            />
+            <span class="error-message"></span>
+          </div>
+        </div>
+        <div class="register-form-item">
+          <label for="confirm_password">Confirm Password</label>
+          <div class="register-form-input-cont">
+            <input
+              type="password"
+              class="register-form-item-input"
+              name="confirm_password"
+              id="confirm_password"
+            />
+            <span class="error-message"></span>
+          </div>
+        </div>
+        <button>Submit</button>
+      </form>
+    </main>
+  </body>
 </html>
