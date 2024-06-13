592813097222ef7643cb8e47de6177c2aefc9cff
joshuaobare <jobare.obare@gmail.com>
2024-06-13 17:49:27 +0300

    Add event listener on email input

--- dist/src/register.js
+++ dist/src/register.js
@@ -1,9 +1,20 @@
 const inputs = document.querySelectorAll("input");
 const form = document.getElementById("register-form");
-const first_name = document.getElementById("first_name");
-const last_name = document.getElementById("last_name");
-const email = document.getElementById("email");
-const date_of_birth = document.getElementById("date_of_birth");
-const password = document.getElementById("password");
-const confirm_password = document.getElementById("confirm_password");
+const first_name = (document.getElementById("first_name"));
+const first_name_error = document.getElementById("first_name_error");
+const last_name = (document.getElementById("last_name"));
+const last_name_error = document.getElementById("last_name_error");
+const email = (document.getElementById("email"));
+const email_error = document.getElementById("email_error");
+const date_of_birth = (document.getElementById("date_of_birth"));
+const date_of_birth_error = document.getElementById("date_of_birth");
+const password = (document.getElementById("password"));
+const password_error = document.getElementById("password_error");
+const confirm_password = (document.getElementById("confirm_password"));
+const confirm_password_error = document.getElementById("confirm_password_error");
+email === null || email === void 0 ? void 0 : email.addEventListener("input", (e) => {
+    if (email.validity.valid) {
+        email_error.textContent = "";
+    }
+});
 //# sourceMappingURL=register.js.map
--- src/register.ts
+++ src/register.ts
@@ -1,10 +1,38 @@
 const inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
 const form: HTMLElement | null = document.getElementById("register-form");
-const first_name: HTMLElement | null = document.getElementById("first_name");
-const last_name: HTMLElement | null = document.getElementById("last_name");
-const email: HTMLElement | null = document.getElementById("email");
-const date_of_birth: HTMLElement | null =
+const first_name: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("first_name")
+);
+const first_name_error: HTMLElement | null =
+  document.getElementById("first_name_error");
+const last_name: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("last_name")
+);
+const last_name_error: HTMLElement | null =
+  document.getElementById("last_name_error");
+const email: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("email")
+);
+const email_error: HTMLElement | null = document.getElementById("email_error");
+const date_of_birth: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("date_of_birth")
+);
+const date_of_birth_error: HTMLElement | null =
   document.getElementById("date_of_birth");
-const password: HTMLElement | null = document.getElementById("password");
-const confirm_password: HTMLElement | null =
-  document.getElementById("confirm_password");
+const password: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("password")
+);
+const password_error: HTMLElement | null =
+  document.getElementById("password_error");
+const confirm_password: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("confirm_password")
+);
+const confirm_password_error: HTMLElement | null = document.getElementById(
+  "confirm_password_error"
+);
+
+email?.addEventListener("input", (e: Event) => {
+  if (email.validity.valid) {
+    email_error!.textContent = "";
+  }
+});
