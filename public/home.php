763dba2c8b5be25ed316eb7444d57afcf62bef9f
joshuaobare <jobare.obare@gmail.com>
2024-06-15 18:57:23 +0300

    Add Google material icons to homepage sidebar

--- public/home.php
+++ public/home.php
@@ -1,27 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+    />
     <link rel="stylesheet" href="../styles/main.css" />
-    <link rel="stylesheet" href="../styles/home.css" />  
+    <link rel="stylesheet" href="../styles/home.css" />
     <title>Home</title>
-</head>
-<body>
+  </head>
+  <body>
     <div class="home">
-        <main class="home-main">
-            <nav class="home-nav">
-                <div class="home-nav-items-cont">
-                    <div class="home-nav-item">Home</div>
-                    <div class="home-nav-item">Budgets</div>
-                    <div class="home-nav-item">Goals</div>
-                    <div class="home-nav-item">Investments</div>
-                    <div class="home-nav-item">Transactions</div>
-                    <div class="home-nav-item">Spending</div>
-                </div>
-            </nav>
-            <div></div>
-        </main>
+      <main class="home-main">
+        <nav class="home-nav">
+          <div class="home-nav-items-cont">
+            <div class="home-nav-item">
+              <span class="material-symbols-outlined home-icon"> home </span>
+              <span>Home</span>
+            </div>
+            <div class="home-nav-item">
+              <span class="material-symbols-outlined home-icon">
+                request_quote
+              </span>
+              <span>Budget</span>
+            </div>
+            <div class="home-nav-item">
+              <span class="material-symbols-outlined home-icon">
+                check_circle
+              </span>
+              <span>Goal</span>
+            </div>
+            <div class="home-nav-item">
+              <span class="material-symbols-outlined home-icon">
+                currency_bitcoin
+              </span>
+              <span>Investments</span>
+            </div>
+            <div class="home-nav-item">
+              <span class="material-symbols-outlined home-icon">
+                payments
+              </span>
+              <span>Transactions</span>
+            </div>
+            <div class="home-nav-item">
+              <span class="material-symbols-outlined home-icon">
+                shopping_cart
+              </span>
+              <span>Spending</span>
+            </div>
+          </div>
+        </nav>
+        <div></div>
+      </main>
     </div>
-</body>
+  </body>
 </html>
