02e1a4125ca79c4dd428df38d8d3ba51b77f3037
joshuaobare <jobare.obare@gmail.com>
2024-06-16 22:22:25 +0300

    Create navbar module layout

--- dist/src/navbar.js
+++ dist/src/navbar.js
@@ -1,7 +1,11 @@
 const navbar = document.createElement("nav");
+navbar.classList.add("main-nav");
 navbar.setAttribute("background-color", "black");
+navbar.setAttribute("width", "100%");
 const navbarLeft = document.createElement("div");
+navbarLeft.textContent = "FINTRACK";
 const navbarMid = document.createElement("div");
 const navbarRight = document.createElement("div");
+navbar.append(navbarLeft, navbarMid, navbarRight);
 export default navbar;
 //# sourceMappingURL=navbar.js.map
