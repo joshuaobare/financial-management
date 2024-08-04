0d13de3f38c96bdca41a074be17f6df789108790
joshuaobare <jobare.obare@gmail.com>
2024-08-04 18:49:20 +0300

    Update navbar styling and add new font

--- src/components/navbar.ts
+++ src/components/navbar.ts
@@ -1,10 +1,15 @@
-const navbar: HTMLElement = document.createElement("div");
+import "../../styles/navbar.css";
 
-const navbarLeft = document.createElement("img");
-navbarLeft.src = "../public/assets/bwlogo.png";
+const navbar: HTMLElement = document.createElement("div");
+navbar.className = "navbar";
+
+const navbarLeft = document.createElement("div");
+navbarLeft.textContent = "FINTRACK";
 navbarLeft.className = "navbar-left";
 const navbarMid = document.createElement("div");
+navbarMid.className = "navbar-mid";
 const navbarRight = document.createElement("div");
+navbarRight.className = "navbar-right";
 navbar.append(navbarLeft, navbarMid, navbarRight);
 
 export { navbar };
