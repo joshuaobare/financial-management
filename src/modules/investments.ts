57ff00410672af3b96c5307c5a56b82209b75b8b
joshuaobare <jobare.obare@gmail.com>
2024-08-05 20:34:45 +0300

    Add assets available in Investments module

--- src/modules/investments.ts
+++ src/modules/investments.ts
@@ -1,5 +1,16 @@
 const createInvestmentsModule = () => {
   const investments = document.createElement("div");
+  const assets = document.createElement("select");
+  const options = ["S&P 500", "Bitcoin", "Ethereum", "Gold", "Crude Oil"];
+
+  options.forEach((option) => {
+    const asset = document.createElement("option");
+    asset.textContent = option;
+    asset.value = option;
+
+    assets.appendChild(asset);
+  });
+  investments.appendChild(assets);
 
   return investments;
 };
